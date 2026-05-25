05eb949d86f96fcaf6bf8aa743bdb2c6106bba33
Pham Anh Tuan <53900662+tuanpa2295@users.noreply.github.com>
2026-05-25 18:27:45 +0700

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count = [0] * 26
+
+        for c in magazine:
+            count[ord(c) - ord('a')] += 1
+
+        for c in ransomNote:
+            idx = ord(c) - ord('a')
+            count[idx] -= 1
+            if count[idx] < 0:
+                return False
+        return True 
